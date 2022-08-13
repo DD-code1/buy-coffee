78ddfd9eceb5785570bcdb755c8285d2089d0f99
DD-code1 <dadsonkojoduke@gmail.com>
2022-08-13 19:51:14 +0000

    first commit contract

---
+++ components/Navbar.tsx
@@ -0,0 +1,25 @@
+const Navbar = () => {
+  return (
+    <div className='flex justify-around items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      <ul className=' hidden md:flex'>
+        <li className='p-4'>Home</li>
+        <li className='p-4'>Company</li>
+        <li className='p-4'>Resources</li>
+        <li className='p-4'>About</li>
+        <li className='p-4'>Contact</li>
+      </ul>
+     
+      <ul className='flex fixed border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+          <li className='p-4 border-b border-gray-600'>Home</li>
+          <li className='p-4 border-b border-gray-600'>Company</li>
+          <li className='p-4 border-b border-gray-600'>Resources</li>
+          <li className='p-4 border-b border-gray-600'>About</li>
+          <li className='p-4'>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        sm: ['14px', '20px'],
+        base: ['16px', '24px'],
+        lg: ['20px', '28px'],
+        xl: ['24px', '32px'],
+      },
+    },
   },
   plugins: [],
 }
